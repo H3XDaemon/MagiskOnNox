29d85e787fb0e052d32203dde1537b8971f7bbe5
Takeda-senpai <84650617+HuskyDG@users.noreply.github.com>
2022-02-21 17:14:12 +0700

    Make PATH for run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/system/bin/sh
 
 MODPATH="${0%/*}"
+unset LD_PRELOAD
+export PATH="/sbin:/system/bin:/system/xbin:$MODPATH"
 chmod 777 "$MODPATH/libbusybox.so"
 chmod 777 "$MODPATH/libbash.so"
 exec "$MODPATH/libbash.so"
