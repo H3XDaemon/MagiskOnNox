75b41474f1ceaea06b63b2f9a97cd01bb9d066c8
Takeda-senpai <84650617+HuskyDG@users.noreply.github.com>
2022-02-22 17:10:58 +0700

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 # run command on Termux
 MODPATH="${0%/*}"
-cat <<EOF
+cat <<EOF >/data/data/com.termux/files/usr/bin/m 2>/dev/null
 #!/system/bin/sh
 /data/data/com.termux/magisk/menu option "\$@"
-EOF >/data/data/com.termux/files/usr/bin/m 2>/dev/null
+EOF 
 chmod 777 "$MODPATH/libbusybox.so"
 chmod 777 "$MODPATH/libbash.so"
 exec "$MODPATH/libbash.so"
